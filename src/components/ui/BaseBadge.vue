5823f19da9f4eecf61b415745680c10a6ca534dd
DangTinhKimNguyen <kimnguyen0491@gmail.com>
2025-12-17 16:08:13 +0700

    component button and badge

---
+++ src/components/ui/BaseBadge.vue
@@ -0,0 +1,79 @@
+<script setup>
+    const props = defineProps({
+        variant: {
+            type: String,
+            required: true // // status | priority | type
+        },
+        value: {
+            type: String,
+            required: true
+        }
+    })
+</script>
+
+<template>
+    <span class="span" :class="[`badge--${variant}`, `${variant}--${value}`]">
+        <slot>{{ value }}</slot>
+    </span>
+</template>
+
+<style scoped>
+    .span {
+        padding: 8px 4px;
+        border-radius: 4px;
+        font-size: 12px;
+        display: inline-flex;
+        align-items: center;
+        text-transform: capitalize;
+    }
+
+    /* ===== STATUS ===== */
+    .status--open {
+        color: #64748B;
+        background-color: #ECEFF1;
+    }
+    .status--working {
+        color: #3B82F6;
+        background-color: #E3F2FD;
+    }
+    .status--pending {
+        color: #F59E0B;
+        background-color: #FFF8E1;
+    }
+    .status--completed {
+        color: #10B981;
+        background-color: #E0F2F1;
+    }
+
+    /* ===== PRIORITY ===== */
+    .priority--high{
+        color: #EF4444;
+        background-color: #FDECEA;
+    }
+    .priority--medium{
+        color: #F97316;
+        background-color: #FFF3E0;
+    }
+    .priority--low{
+        color: #84CC16;
+        background-color: #E8F5E9;
+    }
+
+    /* ===== TYPE ===== */
+    .type--task{
+        color: #64748B;
+        background-color: #ECEFF1;
+    }
+    .type--feature{
+        color: #1D4ED8;
+        background-color: #E3F2FD;
+    }
+    .type--bug{
+        color: #DC2626;
+        background-color: #FCE4EC;
+    }
+    .type--Enhancemant{
+        color: #6D28D9;
+        background-color: #EDE7F6;
+    }
+</style>
---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,49 @@
+<script setup>
+    const props = defineProps({
+        typeButton: {
+            type: String,
+            default: 'primary'
+        }
+    }) 
+</script>
+
+<template>
+    <button class="btn" :class="props.typeButton">
+        <slot name="icon"></slot>
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+    .btn{
+        display: inline-flex;
+        align-items: center;
+        gap: 8px;
+        padding: 12px 12px;
+        border-radius: 6px;
+        cursor: pointer;
+        border: 1px solid transparent;
+    }
+
+    .primary {
+        color: #fff;
+        background: #00BFA5;
+    }
+
+    .outline {
+        color: #00BFA5;
+        background-color: #fff;
+        border: 1px solid #00BFA5;
+    }
+
+    .danger {
+        color: #fff;
+        background-color: #F44336;
+    }
+
+    .cancel {
+        color: #757575;
+        background-color: #fff;
+        border: 1px solid #e0e0e0;
+    }
+</style>
