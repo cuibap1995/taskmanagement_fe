3163c1a5c8f03c2176a9f457db58ed1808a7d53f
DangTinhKimNguyen <kimnguyen0491@gmail.com>
2025-12-18 08:54:23 +0700

    fix-component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,9 @@
-<script setup>
-  import BaseButton from './components/ui/BaseButton.vue'
-  import BaseBadge from './components/ui/BaseBadge.vue'
+<script >
+
 </script>
 
 <template>
-  <div>
-    <section>
-    <h1>Các loại Button</h1>
-    <div>
-      <BaseButton typeButton="primary">+ Button Label</BaseButton>
-      <BaseButton typeButton="outline">+ Button Label</BaseButton>
-      <BaseButton typeButton="danger">+ Button Label</BaseButton>
-      <BaseButton typeButton="cancel">+ Button Label</BaseButton>
-  </div>
-  </section>
 
-  <section>
-    <h1>Các loại Badge</h1>
-    <div>
-      <BaseBadge variant="priority" value="high"></BaseBadge>
-      
-      <BaseBadge variant="type" value="bug"></BaseBadge>
-
-      <BaseBadge variant="status" value="completed"></BaseBadge>
-    </div>
-  </section>
-  </div>
-  
-  
 </template>
 
 <style></style>
