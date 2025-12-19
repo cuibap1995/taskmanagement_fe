c6107c7e0f885a4f437f3354065ea23f37916f19
DESKTOP-KALLT46thanh <quannt@sonix.com.vn>
2025-12-19 08:41:00 +0700

    fix(vite): enable polling to detect file changes in Docker

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   server: {
     host: true,
     port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+      interval: 300,
+    },
     proxy: {
       "/api": {
         target: "http://backend:8000",
@@ -21,8 +23,6 @@ export default defineConfig({
     },
   },
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) },
   },
 })
